a1e6a755bccf6fd6e6b65f3b6d2e0aa3e498acec
Mauricio Chirino <m.chirino89@gmail.com>
2014-03-31 14:17:11 -0430

    Probando insercion y recuperado de datos por bajo

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -4,8 +4,6 @@
  * and open the template in the editor.
  */
 
-
-
 import com.couchbase.client.CouchbaseClient;
 import com.couchbase.client.CouchbaseConnectionFactoryBuilder;
 import java.io.IOException;
@@ -21,7 +19,12 @@ import java.util.logging.Logger;
  * @author mauriciochirino
  */
 public class TestClass {
-    public static void main(String[] args){
+
+    public static final int EXP_TIME = 10;
+    public static final String KEY = "user3";
+    public static final String VALUE = "18847834";
+
+    public static void main(String[] args) {
         CouchbaseConnectionFactoryBuilder cfb = new CouchbaseConnectionFactoryBuilder();
         cfb.setOpTimeout(10000);//10 seconds for operation timeout
         cfb.setTimeoutExceptionThreshold(1500);
@@ -30,16 +33,24 @@ public class TestClass {
         try {
             server = new URI("http://172.16.16.129:8091/pools");
         } catch (URISyntaxException ex) {
-            
+
         }
         List<URI> serverList = new ArrayList<URI>();
         serverList.add(server);
         CouchbaseClient client = null;
         try {
-            client = new CouchbaseClient(cfb.buildCouchbaseConnection(serverList, "default", ""));
+            client = new CouchbaseClient(serverList, "billetera", "");
+            
         } catch (IOException ex) {
-            Logger.getLogger(TestClass.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(TestClass.class.getName()).log(Level.SEVERE, "No se pudo operar sobre el objeto a bd", ex);
         }
-        Object obj = client.get("test");
+        
+        System.out.println("\n" + client.get(KEY).toString() + "\n");
+        
+        //client.set(KEY, EXP_TIME, new user("18839634"));
+        // Store a Document
+
+        //client.set("my-first-document", "Hello Couchbase!").get();
+        // Retreive the Document and print it
     }
 }
